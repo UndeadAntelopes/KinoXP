118d8d0f663fe6ff4fdd51177a4972b35b7dbfe8
Nesrin Saxeide Kristensen <nesrin.tufan@gmail.com>
2020-10-02 15:08:08 +0200

    cleaned up controller

--- src/main/java/kinoxp/kino/controller/MovieController.java
+++ src/main/java/kinoxp/kino/controller/MovieController.java
@@ -4,7 +4,10 @@ import kinoxp.kino.model.Movie;
 import kinoxp.kino.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -20,7 +23,6 @@ public class MovieController {
 
     }
 
-    @GetMapping
 
 
 
